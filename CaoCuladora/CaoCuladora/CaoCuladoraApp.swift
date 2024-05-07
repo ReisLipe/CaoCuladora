d0ac663198923f1093f15d8eaf4b39383b967bca
Filipe Reis <reislipe@protonmail.com>
2024-05-07 16:12:28 -0300

    Adiciona arquivos do projeto

---
+++ CaoCuladora/CaoCuladora/CaoCuladoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaoCuladoraApp.swift
+//  CaoCuladora
+//
+//  Created by Joao Filipe Reis Justo da Silva on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CaoCuladoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
