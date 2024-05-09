ddd6c06829ab88f9ce7714882e47c4364f0e9e37
Filipe Reis <reislipe@protonmail.com>
2024-05-09 17:50:02 -0300

    Implementa segmented control e cálculo de idade

--- CaoCuladora/CaoCuladora/ContentView.swift
+++ CaoCuladora/CaoCuladora/ContentView.swift
@@ -13,19 +13,22 @@ struct ContentView: View {
     // para que o SwiftUi entenda que ela deve ser mantida.
     
     // Esse @ na frente de State é um wrapper. Eu ainda não sei o que é isso :)
-    @State var anosInput: Int? = nil
-    @State var mesesInput: Int? = nil
+    @State var yearsInput: Int? = nil
+    @State var monthsInput: Int? = nil
     @State var result: Int?
+    @State var porte: String = "Pequeno"
+    let portes = ["Grande", "Médio", "Pequeno"]
     
     
     var body: some View {
         VStack(alignment: .leading) {
             Text("Qual a idade do seu cão?")
                 .font(.system(size: 24))
+            
             Text("Anos")
             TextField(
                 "Digite a idade em anos completos",
-                value: $anosInput,
+                value: $yearsInput,
                 format: .number
             )
             .padding()
@@ -33,31 +36,42 @@ struct ContentView: View {
             Text("Meses")
             TextField(
                 "E quatos meses ele tem?",
-                value: $mesesInput,
+                value: $monthsInput,
                 format: .number
             )
             .padding()
             
             Text("Porte")
-            // Segmented control aqui
+            Picker("Porte do cão", selection: $porte){
+                ForEach(portes, id: \.self){ porte in
+                    // Texto é o que estará escrito e tag é o valor que ele atribuirá a variável
+                    // em evidência
+                    Text(porte)
+                        .tag(porte)
+                }
+            }
+            .pickerStyle(.segmented)
+            
+            Divider()
             
             if let result {
                 Text("Seu cachorro tem em idade humana...")
                 Text("\(result) anos")
+                    .font(.system(size: 40))
             } else {
                 Image(ImageResource.clarinha)
                     .resizable() // quero que a imagem redimensione
                     .scaledToFit() // quero sempre mantenha suas proporções originais
                     .frame(maxHeight: 150)
                     .frame(maxWidth: .infinity)
-                .shadow(radius: 20)
+                    .shadow(radius: 20)
+                    .padding()
             }
             
+            
+            
             Button(
-                action: {
-                    result = 23
-                },
-                label: {
+                action: processYears, label: {
                     ZStack{
                         Color.indigo
                         Text("Cãocular")
@@ -72,6 +86,26 @@ struct ContentView: View {
         .bold()
         .fontDesign(.rounded)
         .padding()
+    }
+    
+    func processYears() -> Void {
+        guard
+            let yearsInput,
+            let monthsInput
+        else {
+            // Caso anosInput seja um nil e o else não é o ultimo elemento da função, um
+            // return deve ser adicionado para que o funcionamento da função seja enterrompido.
+            print("Campo de entrada não preenchido!")
+            return Void()
+        }
+        
+        guard yearsInput > 0 || monthsInput > 0 else {
+            print("Algum campo tem que ser maior que zero!")
+            return Void()
+        }
+        
+        // meu resultado vai ser years * 7 mais a fração de meses do ano * 7
+        result = yearsInput * 7 + (monthsInput * 7) / 12
     }
 }
 
